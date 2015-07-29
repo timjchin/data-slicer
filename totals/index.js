a45bc1207d8520402a30ee6978e4d5baeac43347
Tim Chin <timjchin@gmail.com>
2015-07-28 23:01:24 -0400

    nested uniqueBy fix

--- totals/index.js
+++ totals/index.js
@@ -1,39 +1,38 @@
-/**   dataMap = {
-  *     aggregations: [
-  *       {
-  *         dataObject: function () {},
-  *         field: 'fieldname'
-  *       }
-  *     ],
-  *     sortBy: function () {
-  *       
-  *     },
-  *     nested: {
-  *       field: 'field',
-  *       aggregations: []
-  *     }
-  *   };
-  */
+/**   
+ *
+ * Internal Data aggregation nested data structure
+ * dataAggregation = {
+ *     aggregations: [
+ *       {
+ *         dataObject: function () {},
+ *         field: 'fieldname'
+ *       }
+ *     ],
+ *     field: undefined || field to nest by
+ *     nested: undefined || dataAggregation
+ *   };
+ */
 function Totals(opts) {
-  this._uniqueBy = []; 
-  this._fns = [];
-
-  this._outDataFormat = {
-    aggregations: [],
-    sortBy: undefined,
-    nested: undefined
-  };
+  this._outDataFormat = Totals.buildAggregationLevel();
 
   this._currentNestedLevel = this._outDataFormat;
 
   for (var key in Totals.functions) { 
     this[key] = function (key, field) {
-      this._fns.push({ 
+      this._currentNestedLevel.aggregations.push({
         dataObject: Totals.functions[key],
         field: field
       });
       return this;
     }.bind(this, key);
+  }
+}
+
+Totals.buildAggregationLevel = function (field) {
+  return {
+    aggregations: [],
+    nested: undefined,
+    field: field
   }
 }
 
@@ -47,58 +46,106 @@ Totals.functions = {
 
 
 Totals.prototype = {
+  /**
+   *  @method _setData
+   *  @private
+   *  @param data {array|Objects} - data, set by aggregator
+   */
   _setData: function (data) {
     this.data = data;
   },
+
+  /**
+   *  For each key that you call uniqueBy on, aggregations at a specific
+   *  layer will be added together, allowing you to get unique counts for a
+   *  specific nested combination of values. Can be nested as many times as desired.
+   *
+   *  @method uniqueBy
+   *  @public
+   *  @param field {string} data key to nest on
+   */
   uniqueBy: function (field) {
-    this._uniqueBy = field;
+    this._currentNestedLevel.nested = Totals.buildAggregationLevel(field);
+    this._currentNestedLevel = this._currentNestedLevel.nested;
     return this;
   },
+
+  /**
+   *  Data is looped through once, and for each record, we loop through the
+   *  dataAggregation structure. Based on the state of the data going out, we can
+   *  initialize a dataset, and then process each dataset.
+   *
+   *  The aggregations are stored in arrays for ease of looping, and then cleaned up 
+   *  in the postProcessing loop.
+   *
+   *  @method process
+   */
   process: function () {
-    if (this._uniqueBy) { 
-      this._processUniques();
-    } else {
-      this._processSet(this.data);
-    }
-  },
-
-  _processUniques: function () {
-    var uniqueAggs = {};
-
-    for (var i = 0; i < this.data.length; i++) { 
-      var record = this.data[i];
-      var uniqueValue = record[this._uniqueBy];
-      if (!uniqueAggs[uniqueValue]) {
-        uniqueAggs[uniqueValue] = this._initDataSet();
-      }
-      var uniqueDataSet = uniqueAggs[uniqueValue];
-      this._processDataSet(uniqueDataSet, record);
-    }
-
     var outData = {};
-    for (var key in uniqueAggs) {
-      this._postProcessDataSet(uniqueAggs[key])
-      outData[key] = this._generateSetData(uniqueAggs[key]);
+    var toPostProcess = [];
+
+    for (var i = 0; i < this.data.length; i++) {
+
+      var record = this.data[i];
+
+      var currentNested = this._outDataFormat;
+      var currentOutData = outData;
+
+      while (currentNested) {
+        if (currentNested.field) {
+          var field = currentNested.field;
+          var recordFieldValue = record[field];
+
+          if (!currentOutData[field]) {
+            currentOutData[field] = {};
+          }
+
+          if (!currentOutData[field][recordFieldValue]) { 
+            currentOutData[field][recordFieldValue] = {};
+            if (currentNested.aggregations.length) {
+              currentOutData[field][recordFieldValue] = this._initDataSet(currentNested.aggregations);
+              toPostProcess.push({
+                parentDataSet: currentOutData[field],
+                fieldValue: recordFieldValue,
+                aggs: currentNested.aggregations
+              });
+            }
+          }
+
+          if (currentNested.aggregations.length) {
+            var currDataSet = currentOutData[field][recordFieldValue];
+            this._processDataSet(currentNested.aggregations, currDataSet, record);
+          }
+
+          currentOutData = currentOutData[field][recordFieldValue];
+
+        }
+        currentNested = currentNested.nested;
+      }
     }
+
+    for (var i = 0; i < toPostProcess.length; i++) { 
+      var postProcess = toPostProcess[i];
+      var currentOutDataLayer = postProcess.parentDataSet[postProcess.fieldValue];
+      this._postProcessDataSet(postProcess.aggs, currentOutDataLayer);
+      postProcess.parentDataSet[postProcess.fieldValue] = this._generateSetData(currentOutDataLayer, postProcess.aggs);
+    }
+
     return outData;
-
   },
 
-  _processSet: function (set) {
-    var dataSet = this._initDataSet()
-    
-    for (var i = 0; i < set.length; i++) { 
-      this._processDataSet(dataSet, set[i]);
-    }
-
-    this._postProcessDataSet(dataSet);
-    return this._generateSetData(dataSet);
-  },
-
-  _initDataSet: function () {
+  /**
+   *  Note that each dataSet keeps it's values stored in an object with one key of value.
+   *  This allows the total definiton objects to return numerical and string values.
+   *
+   *  @method _initDataSet
+   *  @private
+   *  @param aggs {Array} 
+   */
+  _initDataSet: function (aggs) {
     var dataSet = [];
-    for (var i = 0; i < this._fns.length; i++) { 
-      var fn = this._fns[i];
+    for (var i = 0; i < aggs.length; i++) { 
+      var fn = aggs[i];
       if (!fn.dataObject.data) throw new Error( 'total function must have a constant data');
       dataSet[i] = {
         value: fn.dataObject.data()
@@ -107,28 +154,28 @@ Totals.prototype = {
     return dataSet;
   },
 
-  _processDataSet: function (dataSet, record) {
-    for (var i = 0; i < this._fns.length; i++) { 
-      var fn = this._fns[i];
+  _processDataSet: function (aggs, dataSet, record) {
+    for (var i = 0; i < aggs.length; i++) { 
+      var fn = aggs[i];
       var value = record[fn.field];
       dataSet[i].value = fn.dataObject.process(value, dataSet[i].value);
     }
   },
 
-  _postProcessDataSet: function (dataSet) {
-    for (var i = 0; i < this._fns.length; i++) { 
-      var fn = this._fns[i];
+  _postProcessDataSet: function (aggs, dataSet) {
+    for (var i = 0; i < aggs.length; i++) { 
+      var fn = aggs[i];
       if (fn.dataObject.postProcess) {
         dataSet[i].value = fn.dataObject.postProcess(dataSet[i].value);
       }
     }
   },
 
-  _generateSetData: function (dataSet) {
+  _generateSetData: function (dataSet, aggs) {
     var obj = {};
     for (var i = 0; i < dataSet.length; i++) { 
       var data = dataSet[i];
-      obj[this._fns[i].dataObject.key] = dataSet[i];
+      obj[aggs[i].dataObject.key] = dataSet[i].value;
     }
     return obj;
   }
