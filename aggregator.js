da58c1f7a923ef6594c01156ce41dfb233bdd1f5
Tim Chin <timjchin@gmail.com>
2015-07-30 21:06:33 -0400

    top level changes, new not required

--- aggregator.js
+++ aggregator.js
@@ -1,8 +1,8 @@
 var DataTransformer = require('./data-transforms/index');
 var Totals = require('./totals/index');
 
-function Aggregator (opts) {
-  this.opts = opts || {};
+function Aggregator () {
+  if (!(this instanceof Aggregator)) return new Aggregator();
   this.dataTransformer = new DataTransformer();
   this.totalFunction = new Totals();
 }
@@ -12,12 +12,19 @@ Aggregator.prototype = {
     this.data = data;
     this.totalFunction._setData(data);
     this.dataTransformer._setData(data);
+    return this;
   },
   transform: function (transform) {
     return this.dataTransformer.transform.apply(this.dataTransformer, arguments);
   },
   totals: function () {
     return this.totalFunction;
+  },
+  modify: function () {
+    return this.dataTransformer.modify();
+  },
+  process: function () {
+    return this.totalFunction.process();
   }
 };
 
